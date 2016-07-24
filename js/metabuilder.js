5ddc9d67c5750860118e7b5373145e942b950e61
CafeSquare <socialify@gmail.com>
2016-07-24 22:12:16 +0200

    Add files via upload

--- js/metabuilder.js
+++ js/metabuilder.js
@@ -160,3 +160,20 @@ $('#browse').css ('display', 'none');
 $('.editorImages').css('display', 'block');
 $('.editorImagesOpen').css('display', 'none');
 });
+
+// UPLOAD IMAGE
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width('100%')
+                    .height('100%');
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
