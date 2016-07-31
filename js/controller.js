cf533cf438f02550ccce257b8146b16e27eb67a5
CafeSquare <socialify@gmail.com>
2016-07-31 15:41:31 +0200

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'the title';
+  $scope.promo = 'the promo';
+  $scope.product = {
+  name: 'The Book of Trees',
+  price: 19
+};
+}]);
